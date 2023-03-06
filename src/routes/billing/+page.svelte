0a0a9366a95f1e4207570583ef1124d2d9f3747e
mechy <rit6569@gmail.com>
2023-03-06 22:19:28 +0530

    init repo-2

---
+++ src/routes/billing/+page.svelte
@@ -0,0 +1,106 @@
+<div>
+    <h1 class=" mt-12 mb-2 text-3xl font-medium leading-tight text-primary ...">Billing</h1>
+    <table class="font-sans mx-auto table-fixed border-collapse border overflow-auto w-3/5 rounded-sm Lato font-light ...">
+        <tbody>
+          <tr class="bg-teal-900 text-white ...">
+            <td>S.no</td>
+            <td>Item</td>
+            <td>Price</td>
+            <td>Quantity</td>
+            <td>Amount</td>
+          </tr>
+          <tr class="odd:bg-teal-50 hover:bg-stone-100 ...">
+            <td class=" border-b border-slate-100 ...">1</td>
+            <td class="border-b border-slate-100 ...">Indianapolis Lorem ipsum dolor excepturi.</td>
+            <td class="border-b border-slate-100 ...">100</td>
+            <td class="border-b border-slate-100 ...">1</td>
+            <td class="border-b border-slate-100 ...">100</td>
+          </tr>
+          <tr class="odd:bg-teal-50 hover:bg-stone-100 ...">
+            <td class="border-b border-slate-100 ...">2</td>
+            <td class="border-b border-slate-100 ...">Indianapolis Lorem ipsum dolor excepturi.</td>
+            <td class="border-b border-slate-100 ...">100</td>
+            <td class="border-b border-slate-100 ...">1</td>
+            <td class="border-b border-slate-100 ...">100</td>
+          </tr>
+          <tr class=" odd:bg-teal-50 hover:bg-stone-100 ...">
+            <td class="border-b border-slate-100 ...">3</td>
+            <td class="border-b border-slate-100 ...">Indianapolis Lorem ipsum dolor excepturi.</td>
+            <td class="border-b border-slate-100 ...">100</td>
+            <td class="border-b border-slate-100 ...">1</td>
+            <td class="border-b border-slate-100 ...">100</td>
+          </tr>
+          <tr class="bg-teal-900 text-white ...">
+            <td colspan="4" class="border-b border-slate-900 text-left ...">Total Amount</td>
+            <td class="border-b border-slate-900 ...">300</td>
+          </tr>
+          <tr class="bg-teal-900 text-white ...">
+            <td colspan="4" class="border-b border-slate-900 text-left ...">Discounted price</td>
+            <td class="border-b border-slate-900 ...">300</td>
+          </tr>
+          <tr class="bg-teal-900 text-white ...">
+            <td colspan="4" class="border-b border-slate-900 p-44 ...">Net amount</td>
+            <td class="border-b border-slate-900 ...">300</td>
+          </tr>
+        </tbody>
+      </table>
+
+      <div class="mx-auto w-3/5 mt-4 ...">
+        <div class="float-left mr-24 ...">
+            <label>Customer Ph Number
+                <input class="mx-autoplaceholder:text-slate-400 bg-white border w-60 ml-4
+                border-slate-300 rounded-md p-2 shadow-sm focus:outline-none focus:border-teal-900
+                focus:ring-teal-900 focus:ring-1 sm:text-sm" placeholder="Phone number" type="text"/> 
+            </label>
+        </div>
+        <div class="block float-left mr-24 ...">
+            <div class="mt-2">
+              <label class="inline-flex items-center">
+                <input type="checkbox" class="w-6 h-6 rounded accent-teal-500"  />
+                <span class="ml-2">Home Delivery</span>
+              </label>
+            </div>
+          </div>
+        <div class="float-left ...">
+            <label>Delivery Charges
+                <input class="mx-autoplaceholder:text-slate-400 bg-white border w-60 ml-4
+                border-slate-300 rounded-md p-2 shadow-sm focus:outline-none focus:border-teal-900
+                focus:ring-teal-900 focus:ring-1 sm:text-sm" placeholder="Delivery Charges" type="text"/> 
+            </label>
+        </div>
+        <div class="clear-both ...">
+        </div>
+        <div class="mt-8 ...">
+            <label>Discount
+                <input class="mx-autoplaceholder:text-slate-400 bg-white border w-60 ml-4
+                border-slate-300 rounded-md p-2 shadow-sm focus:outline-none focus:border-teal-900
+                focus:ring-teal-900 focus:ring-1 sm:text-sm" placeholder="Discount Price" type="text"/> 
+            </label>
+        </div>
+        <div class="mt-8 ...">
+            <label for="address-box" class="block mb-2 ...">Address</label>
+            <textarea id="address-box" rows="4" class="mx-autoplaceholder:text-slate-400 bg-white border w-2/5
+            border-slate-300 rounded-md p-2 shadow-sm focus:outline-none focus:border-teal-900
+            focus:ring-teal-900 focus:ring-1 sm:text-sms" placeholder="Write your thoughts here..."></textarea>
+        </div>
+        <div class="float-right inline-flex items-center justify-center px-4 py-2
+                text-base font-medium leading-6 whitespace-no-wrap border bg-teal-50 border-gray-500 rounded-md shadow-sm
+                hover:bg-teal-900 hover:text-white focus:outline-none focus:shadow-none hover:cursor-pointer">
+            Collect Payment 
+        </div>
+        <div class="float-left inline-flex items-center justify-center px-4 py-2
+                text-base font-medium leading-6 whitespace-no-wrap border bg-teal-50 border-gray-500 rounded-md shadow-sm
+                hover:bg-teal-900 hover:text-white focus:outline-none focus:shadow-none hover:cursor-pointer">
+                Register Member/Deduct points
+        </div>
+    </div>
+</div>
+
+
+
+  <style lang="postcss">
+    td {
+        padding: 0.2em;
+        text-align: center;
+    }
+  </style>
