af40f348312d1215f205bda29e169cdd53ec6521
mechy <rit6569@gmail.com>
2023-03-08 11:26:56 +0530

    Declare checkbox component

--- src/lib/components/Radio.svelte
+++ src/lib/components/Radio.svelte
@@ -2,14 +2,10 @@
 	export let label: string;
 </script>
 
-<div class="inline-flex items-center align-middle ...">
-	<input
-		id="default-checkbox"
-		type="checkbox"
-		value=""
-		class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded
-     focus:ring-teal-500 dark:focus:ring-teal-600
-     dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-	/>
-	<label for="default-checkbox" class="ml-2 text-sm font-medium">{label}</label>
+<div>
+	<div class="mt-2 ...">
+		<input type="radio" name="radio"
+		class="w-4 h-4 bg-slate-50 hover:bg-teal-700 checked:bg-teal-500 cursor-pointer ..." />
+	<label for="radio-three" class="ml-2">{label}</label>
+	</div>
 </div>
---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,1 @@
+<h1>Kori Login</h1>
