7d9a369124dd412e87e36ffdcef2c4a21f02276c
Raz Crimson <52282402+RazCrimson@users.noreply.github.com>
2023-03-19 04:07:54 +0530

    add: generic request maker

---
+++ src/lib/request.ts
@@ -0,0 +1,88 @@
+import { get } from 'svelte/store'
+import { authData } from '$lib/store/store';
+
+
+export interface QueryParams {
+	[key: string]: string | number | boolean | Array<string | number | boolean>;
+}
+
+export interface Headers {
+	[key: string]: string;
+}
+
+export enum HttpMethod {
+    GET = 'GET',
+    POST = 'POST',
+    PUT = 'PUT',
+    DELETE = 'DELETE',
+  }
+  
+  function convertQueryParams(params: QueryParams): string[][] {
+	const result: string[][] = [];
+	for (const [key, value] of Object.entries(params)) {
+		if (Array.isArray(value)) {
+			value.map((val) => result.push([key, String(val)]));
+		} else {
+			result.push([key, String(value)]);
+		}
+	}
+	return result;
+}
+
+export async function httpRequest<T>(
+	method: HttpMethod,
+	url: string,
+	body?: Record<string, string | number | boolean>,
+	queryParams?: QueryParams,
+	headers?: Headers
+): Promise<T> {
+	const convertedParams = queryParams ? convertQueryParams(queryParams) : undefined;
+	const searchParams = new URLSearchParams(convertedParams);
+
+	const requestOptions: RequestInit = {
+		method,
+		headers: {
+			'Content-Type': 'application/json',
+			...headers
+		},
+		body: body ? JSON.stringify(body) : undefined
+	};
+
+	const response = await fetch(`${url}?${searchParams}`, requestOptions);
+
+	if (!response.ok) {
+		throw new Error(`HTTP error ${response.status}`);
+	}
+
+	const responseBody: T = await response.json();
+	return responseBody;
+}
+
+
+export function authenticatedHttpRequest<T>(
+	method: HttpMethod,
+	url: string,
+	body?: Record<string, string | number | boolean>,
+	queryParams?: QueryParams,
+	headers?: Headers
+): Promise<T> {
+    const requestHeaders = {...headers};
+    const authenticationData = get(authData);
+    if (typeof authenticationData.token != 'string' || authenticationData.token == ""){
+		throw new Error(`Invalid Token: ${authenticationData.token}`);
+    }
+
+    requestHeaders["Authorization"] = `BEARER ${authenticationData.token}`
+    return httpRequest(method, url, body, queryParams, requestHeaders)
+}
+
+
+export function defaultHttpRequest<T>(
+	method: HttpMethod,
+	url: string,
+	body?: Record<string, string | number | boolean>,
+	queryParams?: QueryParams,
+	headers?: Headers
+): Promise<T>   {
+	return httpRequest(method, url, body, queryParams, headers)
+}
