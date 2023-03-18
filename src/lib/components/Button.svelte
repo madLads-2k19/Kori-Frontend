35f4cb8b78359a7465dec8d2e47c447844d35c44
Raz Crimson <52282402+RazCrimson@users.noreply.github.com>
2023-03-19 01:04:18 +0530

    fix: linting

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	export let buttonText: string;
-	export let onClick: Function;
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	export let onClick: (event?: Event) => any;
 </script>
 
 <div
@@ -8,6 +9,7 @@
 text-base font-medium leading-6 whitespace-no-wrap border bg-teal-50 border-gray-500 rounded-md shadow-sm
 hover:bg-teal-900 hover:text-white focus:outline-none focus:shadow-none hover:cursor-pointer"
 	on:click={onClick}
+	on:keypress={onClick}
 >
 	{buttonText}
 </div>
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,69 +1,68 @@
 <script lang="ts">
 	import Button from '$lib/components/Button.svelte';
 	import TextInput from '$lib/components/TextInput.svelte';
-    import { authData } from '$lib/store/store.js';
+	import { authData } from '$lib/store/store.js';
 
-    let email: string = "";
-    let password: string = "";
-    let triedLogin: boolean = false
-    let loginStatus: boolean = true
+	let email: string = '';
+	let password: string = '';
+	let triedLogin: boolean = false;
+	let loginStatus: boolean = true;
 
-    function login () {
-        fetch(
-            "https://kori-backend.azurewebsites.net/user/v1/login",
-            {
-                method: "POST",
-                headers: {
-                    "Content-type": "application/json"
-                },
-                body: JSON.stringify({
-                    email,
-                    password
-                })
-            }
-        )
-        .then(response => {
-            triedLogin = true
-            if (!response.ok) {
-                loginStatus = false;
-                throw new Error("Login Failed");
-            }
-            loginStatus = true
-            return response.json()
-        })
-        .then(data => {
-            authData.set({ orgId: data["org_id"], userId: data["user_id"], token: data["token"] });            
-        })
-        .catch(error => {
-            loginStatus = false;
-            console.log(error);
-        });
-    }
+	function login() {
+		fetch('https://kori-backend.azurewebsites.net/user/v1/login', {
+			method: 'POST',
+			headers: {
+				'Content-type': 'application/json'
+			},
+			body: JSON.stringify({
+				email,
+				password
+			})
+		})
+			.then((response) => {
+				triedLogin = true;
+				if (!response.ok) {
+					loginStatus = false;
+					throw new Error('Login Failed');
+				}
+				loginStatus = true;
+				return response.json();
+			})
+			.then((data) => {
+				authData.set({ orgId: data['org_id'], userId: data['user_id'], token: data['token'] });
+			})
+			.catch((error) => {
+				loginStatus = false;
+				console.log(error);
+			});
+	}
 </script>
 
 <div class="h-screen flex bg-teal-800 ...">
 	<div class="my-auto border border-slate-200 mx-auto bg-white rounded-md ...">
-        <div class="p-8">
-            <p class="text-5xl text-center mb-4 text-teal-900 ...">Kori</p>
-            <p class="text-xl text-center mb-4 ...">Welcome</p>
-            <p class="text-center mb-2">Login to use Kori</p>
-            <div class="mb-4">
-                <p class="text-gray-500">Email</p>
-                <TextInput bind:value={email} type="email" placeholder="Enter your email"/>    
-            </div>
-            <div class="mb-8">
-                <p class="text-gray-500">Password</p>
-                <TextInput bind:value={password} type="password" placeholder="Enter your password"/>
-            </div>
-            <div class="text-center mb-4">
-                <Button buttonText="Login" onClick={login}></Button>    
-            </div>
-            {#if triedLogin && !loginStatus}
-                <p class="text-red-800 text-center mb-2">Incorrect email or password!</p>
-            {:else if triedLogin && loginStatus}
-                <p class="text-green-800 text-center mb-2">Login success!</p>
-            {/if}
-            <p class="text-center">Not a user? <a class="text-teal-900 font-semibold underline" href="register">Register</a></p>
-        </div>
-    </div>
+		<div class="p-8">
+			<p class="text-5xl text-center mb-4 text-teal-900 ...">Kori</p>
+			<p class="text-xl text-center mb-4 ...">Welcome</p>
+			<p class="text-center mb-2">Login to use Kori</p>
+			<div class="mb-4">
+				<p class="text-gray-500">Email</p>
+				<TextInput bind:value={email} type="email" placeholder="Enter your email" />
+			</div>
+			<div class="mb-8">
+				<p class="text-gray-500">Password</p>
+				<TextInput bind:value={password} type="password" placeholder="Enter your password" />
+			</div>
+			<div class="text-center mb-4">
+				<Button buttonText="Login" onClick={login} />
+			</div>
+			{#if triedLogin && !loginStatus}
+				<p class="text-red-800 text-center mb-2">Incorrect email or password!</p>
+			{:else if triedLogin && loginStatus}
+				<p class="text-green-800 text-center mb-2">Login success!</p>
+			{/if}
+			<p class="text-center">
+				Not a user? <a class="text-teal-900 font-semibold underline" href="register">Register</a>
+			</p>
+		</div>
+	</div>
 </div>
