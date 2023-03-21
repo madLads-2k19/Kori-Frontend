614fc5d93809d469f60caaa7bc47e87adcc23c17
Sheerabth <sheerabth1610@gmail.com>
2023-03-21 11:40:45 +0530

    fix: local store

--- src/lib/localStore.ts
+++ src/lib/localStore.ts
@@ -1,4 +1,28 @@
 import { authData } from '$lib/store';
 import { get } from 'svelte/store';
 
-export const userData = get(authData);
+class LocalStore {
+	get name() {
+		return get(authData).name;
+	}
+	get email() {
+		return get(authData).email;
+	}
+	get permission_level() {
+		return get(authData).permission_level;
+	}
+	get default_store_id() {
+		return get(authData).default_store_id;
+	}
+	get org_id() {
+		return get(authData).org_id;
+	}
+	get user_id() {
+		return get(authData).user_id;
+	}
+	get_token() {
+		return get(authData).token;
+	}
+}
+
+export const userData = new LocalStore();
